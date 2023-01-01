042baeb6ca96cb0a649347e44542ade40dcfcd42
Oleksandr Shmatko <blacknorfolk@gmail.com>
2023-01-01 18:01:53 +0200

    add promises generator

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  let delay = Number(formEl.elements.delay.value);
+  const step = Number(formEl.elements.step.value);
+  const amount = Number(formEl.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(result => {
+        console.log(
+          `✅ Fulfilled promise ${result.position} in ${result.delay}ms`
+        );
+        Notify.success(
+          `✅ Fulfilled promise ${result.position} in ${result.delay}ms`,
+          { timeout: 1000 }
+        );
+      })
+      .catch(result => {
+        console.log(
+          `❌ Rejected promise ${result.position} in ${result.delay}ms`
+        );
+        Notify.failure(
+          `❌ Rejected promise ${result.position} in ${result.delay}ms`,
+          { timeout: 1000 }
+        );
+      });
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolved, rejected) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolved({ position, delay });
+      } else {
+        rejected({ position, delay });
+      }
+    }, delay);
+  });
 }
